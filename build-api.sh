9dc4dceab8223d7ad8a7f7023655e833e6358804
Kiwi <jallanremy@gmail.com>
2017-03-18 13:20:30 +0100

    try

--- build-api.sh
+++ build-api.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 docker-compose down
+echo "Build..."
 docker build -t devops/bank-api -f Dockerfile.api .
-cp docker-compose.api.yml docker-compose.yml
-docker-compose up -d
+#cp docker-compose.api.yml docker-compose.yml
+echo "Compose..."
+docker-compose -f docker-compose.api.yml up -d
 echo "Api launch"
-rm docker-compose.yml
+#rm docker-compose.yml
