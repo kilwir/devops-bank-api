0396dffd36f75318e8c6e01d0ae0440a4e84406a
Kiwi <jallanremy@gmail.com>
2017-03-18 13:04:02 +0100

    fix build-api

--- build-api.sh
+++ build-api.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker-compose -f docker-compose.api.yml down
+docker-compose down
 docker build -t devops/bank-api -f Dockerfile.api .
 cp docker-compose.api.yml docker-compose.yml
 docker-compose up -d
